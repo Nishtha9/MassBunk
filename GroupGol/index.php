8ffd093df405d9cd38c91da132ebcebad8b2180b
Nishtha9 <Nish0349@gmail.com>
2018-04-01 22:20:16 +0530

    Changed titles

--- GroupGol/index.php
+++ GroupGol/index.php
@@ -1,7 +1,7 @@
 <html>
     
           <head>
-                <title> group_gol!</title>
+                <title>Group Gol</title>
                  <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css">
